5c2dd6db3ebe6c09b17f80e36eb6010145e3759e
Anderson <acarneirosousa@gmail.com>
2021-02-10 23:49:36 -0300

    added shffledEmails and Map with answers

--- src/app.ts
+++ src/app.ts
@@ -1,43 +1,82 @@
 import emails from '../Files/emails';
 import items from '../Files/items';
 
-// console.log(emails.map(email => email));
 interface Items {
   itemName: string;
   itemQuantity: number;
   itemPrice: number;
 }
 
-const GetTotalValue = (items: Items): number => {
-  let sum = 0;
-  items.map((item: Items) => {
-    sum += item.itemQuantity * item.itemPrice;
-    console.log(
-      `Quantity = ${item.itemQuantity} * Price= ${item.itemPrice} Sum = ${
-        item.itemQuantity * item.itemPrice
-      } sumAcc = ${sum}`,
-    );
+let isEmptyEmails = false;
+let isEmptyItems = false;
+
+if (emails.length > 0) {
+  isEmptyEmails = false;
+} else {
+  isEmptyEmails = true;
+  console.log('Warning: Empty Email list!');
+}
+
+if (items.length > 0) {
+  isEmptyItems = false;
+} else {
+  isEmptyItems = true;
+  console.log('Warning: Empty Item list!');
+}
+
+if (!isEmptyEmails && !isEmptyItems) {
+  const GetTotalValue = (items: Items): number => {
+    let sum = 0;
+    items.map((item: Items) => {
+      sum += item.itemQuantity * item.itemPrice;
+      console.log(
+        `Quantity = ${item.itemQuantity} * Price= ${
+          item.itemPrice
+        } cents, Sum = ${
+          item.itemQuantity * item.itemPrice
+        } cents, Accumulated Sum = ${sum} cents`,
+      );
+      return sum;
+    });
     return sum;
-  });
-  return sum;
-};
+  };
 
-const totalValue = GetTotalValue(items);
+  const totalValue = GetTotalValue(items);
 
-const GetPricePerEmail = (emails, totalValue) => {
-  const emailsQuantity: number = emails.length;
-  const y = parseInt(totalValue / emailsQuantity);
-  const x = emailsQuantity * (y + 1) - totalValue;
+  function shuffleArray(array) {
+    const array2 = array.slice(0);
+    for (let i = array2.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array2[i], array2[j]] = [array2[j], array2[i]];
+    }
+    return array2;
+  }
 
-  return console.log(
-    `
-    EmailQty = ${emailsQuantity},
-    TotalValue = ${totalValue},
-    Number of people paying ${y} cents:${x} ,
-    Number of people paying ${y + 1} cents: ${emailsQuantity - x}, Total = ${
-      y * x + (y + 1) * (emailsQuantity - x)
-    }`,
-  );
-};
+  const GetPricePerEmail = (emails, totalValue) => {
+    const emailsQuantity: number = emails.length;
+    const y = parseInt(totalValue / emailsQuantity);
+    const x = emailsQuantity * (y + 1) - totalValue;
 
-GetPricePerEmail(emails, totalValue);
+    console.log(
+      `\nEmailQuantity = ${emailsQuantity}, \nTotalValue = ${totalValue} cents, \nNumber of people paying ${y} cents: ${x}, \nNumber of people paying ${
+        y + 1
+      } cents: ${emailsQuantity - x}, \nTotal = ${
+        y * x + (y + 1) * (emailsQuantity - x)
+      } cents.\n`,
+    );
+
+    const shuffledEmails = shuffleArray(emails);
+
+    const answer = new Map();
+
+    shuffledEmails.map((email, index) => {
+      const price = index < x ? y : y + 1;
+      answer.set(email, price);
+      // console.log(email, index, price);
+    });
+
+    return answer;
+  };
+
+  console.log(GetPricePerEmail(emails, totalValue));
+}
